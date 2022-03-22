9db99fdd8777d2d53c880e812871a33f9b5be65a
luane <luaneemiliano@gmail.com>
2022-03-22 10:28:31 -0700

    Initial Commit

---
+++ json/jsonApp.swift
@@ -0,0 +1,17 @@
+//
+//  jsonApp.swift
+//  json
+//
+//  Created by luane Niejelski on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct jsonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
