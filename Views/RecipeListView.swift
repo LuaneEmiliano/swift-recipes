5e828fecc64ce10dd689fbe9dd3e08e3accade2f
luane <luaneemiliano@gmail.com>
2022-03-27 10:24:41 -0700

    - Change swiftUI list to scrollView, lazyVstack and foreach - High navigation bar title and use the element for the little in Recipe View

--- Views/RecipeListView.swift
+++ Views/RecipeListView.swift
@@ -9,32 +9,48 @@ import SwiftUI
 
 struct RecipeListView: View {
     
- @EnvironmentObject var model: RecipeModel
+    @EnvironmentObject var model: RecipeModel
     
     var body: some View {
         
-       NavigationView{
-           
-            List(model.recipes) { r in
-                
-                NavigationLink(
-                destination: RecipeDetailView(recipe: r),
-                 label: {
-                    
-                    //Mark: Row item
-                    HStack(spacing: 20.0) {
-                        Image(r.image)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 50, height: 50, alignment: .center)
-                            .clipped()
-                            .cornerRadius(5)
-                        Text(r.name)
-                    }
-                })
-            }
-            .navigationBarTitle("All Recipes")
+        NavigationView{
             
+            VStack(alignment: .leading) {
+                
+                Text("All Recipes")
+                    .bold()
+                    .padding(.top, 40)
+                    .font(.largeTitle)
+                
+                ScrollView {
+                    
+                    LazyVStack(alignment: .leading){
+                        
+                        ForEach(model.recipes) { r in
+                            
+                            NavigationLink(
+                                
+                                destination: RecipeDetailView(recipe: r),
+                                label: {
+                                    
+                                    //Mark: Row item
+                                    HStack(spacing: 20.0) {
+                                        Image(r.image)
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 50, height: 50, alignment: .center)
+                                            .clipped()
+                                            .cornerRadius(5)
+                                        Text(r.name)
+                                            .foregroundColor(.black)
+                                  }
+                              })
+                          }
+                      }
+                  }
+              }
+            .navigationBarHidden(true)
+            .padding(.leading)
         }
     }
 }
@@ -42,5 +58,6 @@ struct RecipeListView: View {
 struct RecipeListView_Previews: PreviewProvider {
     static var previews: some View {
         RecipeListView()
+            .environmentObject(RecipeModel())
     }
 }
